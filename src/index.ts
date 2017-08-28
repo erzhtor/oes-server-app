53ff5f2551fe02ae6108539c16408f51609d4fca
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2017-08-28 22:27:01 +0300

    Add koa-router with sample example code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import * as Koa from "koa";
+import * as KoaRouter from 'koa-router';
 import * as io from 'socket.io';
-
 const app = new Koa();
 
-app.use((ctx: Koa.Context) => {
-    ctx.body = "Hello Koa!";
+const router = new KoaRouter();
+router
+.get('/', (ctx: Koa.Context) => {
+    ctx.body = "Main page!";
+}).get('/about', (ctx: Koa.Context) => {
+    ctx.body = "About page!";
+}).get('/redirect', async (ctx: Koa.Context) => {
+    ctx.redirect('/');
 });
+
+app.use(router.routes());
 
 const server = app.listen(3000);
 
