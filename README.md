a44caae22cf19c2bba5832e37958433c91c22e5f
Erzhan Torokulov <eragon.sbt@gmail.com>
2017-10-12 23:30:06 +0300

    Minor changes: add koa-router (both types and npm package); Fix .editorconfig; Fix Readme.md; #fix #change #add

--- README.md
+++ README.md
@@ -9,3 +9,13 @@
 * nodejs: 8.3^
 * koa: 2.3^
 
+
+## Docker command scripts in package.json 
+`npm run docker:build`: build docker image  
+`npm run docker:run`: run docker image (after building)  
+`npm run docker:rm`: remove all containers based on image  
+`npm run docker:rmi`: remove image  
+`npm run docker:stack:init`: init swarm to connect to connect to local swarm  
+`npm run docker:stack:deploy`: start/restart swarm when docker-compose.yml changes (if already deployed)  
+`npm run docker:stack:rm`: down the swarm (leaves single instance of the container)  
+`npm run docker:stack:leave`: down the swarm (all containers)  
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import * as Koa from "koa";
+import * as Koa from 'koa';
 import * as KoaRouter from 'koa-router';
 import * as io from 'socket.io';
 const app = new Koa();
 
 const router = new KoaRouter();
 router
-.get('/', (ctx: Koa.Context) => {
-    ctx.body = "Main page!";
-}).get('/about', (ctx: Koa.Context) => {
-    ctx.body = "About page!";
-}).get('/redirect', async (ctx: Koa.Context) => {
-    ctx.redirect('/');
-});
+    .get('/', (ctx: Koa.Context) => {
+        ctx.body = 'Main page!';
+    }).get('/about', (ctx: Koa.Context) => {
+        ctx.body = 'About page!';
+    }).get('/redirect', async (ctx: Koa.Context) => {
+        ctx.redirect('/');
+    });
 
 app.use(router.routes());
 
