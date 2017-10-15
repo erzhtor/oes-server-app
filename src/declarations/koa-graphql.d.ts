4b88f4c96db6d0855e303baddd89fb5bbc1b9cbb
Erzhan Torokulov <eragon.sbt@gmail.com>
2017-10-15 13:52:14 +0300

    Add config npm package; Change import orders in tslint.json to false; #change #add

--- src/declarations/koa-graphql.d.ts
+++ src/declarations/koa-graphql.d.ts
@@ -3,7 +3,8 @@ declare module 'koa-graphql' {
     import { IMiddleware } from "koa-router";
     interface KoaGraphqlConfig {
         schema: any
-        graphiql: boolean
+        graphiql?: boolean,
+        rootValue?: any,
     }
 
     function graphqlHTTP(config: KoaGraphqlConfig): IMiddleware;
